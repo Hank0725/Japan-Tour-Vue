cb3414de016931ace80112c989f089323a6fab1f
Hank0725 <hank2929@hotmail.com>
2020-04-10 17:28:02 +0800

    0410

---
+++ all.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      copyright:'Copyright©'+new Date().getFullYear()+" Made by Hank"+" 本網站資訊只供示範、教學之用途",
+    }
+  })
